a497433f5d23d63365b765ff1e794569942b8442
Michelle <michelle.mutinda@strathmore.edu>
2025-06-17 12:08:51 +0300

    Updates on Log-in

--- connection.php
+++ connection.php
@@ -3,6 +3,9 @@
     $username="root";
     $password="";
     $db_name="juakazi";
+    $port="3307";
+   
+
     $conn=new mysqli($servername, $username, $password, $db_name, 3307);
     if($conn->connect_error){
       die("Connection failed".$conn->connect_error);   
